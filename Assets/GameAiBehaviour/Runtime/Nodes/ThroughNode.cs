c4ae2fed604830fb3558b4acc3d962897b595ad7
ito_hironori <daitoku1988@gmail.com>
2023-01-15 00:30:18 +0900

    何もしないノード追加

---
+++ Assets/GameAiBehaviour/Runtime/Nodes/ThroughNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace GameAiBehaviour {
+    /// <summary>
+    /// ただ通すだけのノード
+    /// </summary>
+    public sealed class ThroughNode : DecoratorNode {
+        private class Logic : Logic<ThroughNode> {
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            public Logic(IBehaviourTreeController controller, ThroughNode node) : base(controller, node) {
+            }
+
+            /// <summary>
+            /// 実行ルーチン
+            /// </summary>
+            protected override IEnumerator ExecuteRoutineInternal() {
+                // 子がいない場合は失敗
+                if (Node.child == null) {
+                    SetState(State.Failure);
+                    yield break;
+                }
+
+                // 接続先ノードの実行
+                yield return ExecuteNodeRoutine(Node.child, SetState);
+            }
+        }
+
+        /// <summary>
+        /// ロジックの生成
+        /// </summary>
+        public override ILogic CreateLogic(IBehaviourTreeController controller) {
+            return new Logic(controller, this);
+        }
+    }
+}
