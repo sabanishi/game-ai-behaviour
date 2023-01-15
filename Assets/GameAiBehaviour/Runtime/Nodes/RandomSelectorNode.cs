c1675602bd167926fbcd9b3877be8f7e3560bd78
ito_hironori <daitoku1988@gmail.com>
2023-01-16 01:49:51 +0900

    RandomSelector, RandomSequence追加

---
+++ Assets/GameAiBehaviour/Runtime/Nodes/RandomSelectorNode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace GameAiBehaviour {
+    /// <summary>
+    /// ランダム選択用ノード
+    /// </summary>
+    public sealed class RandomSelectorNode : CompositeNode {
+        private class Logic : Logic<RandomSelectorNode> {
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            public Logic(IBehaviourTreeRunner runner, RandomSelectorNode node) : base(runner, node) {
+            }
+
+            /// <summary>
+            /// 実行ルーチン
+            /// </summary>
+            protected override IEnumerator ExecuteRoutineInternal() {
+                // ランダムに実行トライ
+                var orderedIndices = Enumerable.Range(0, Node.children.Length)
+                    .OrderBy(_ => Random.Range(0, 100))
+                    .ToArray();
+                
+                for (var i = 0; i < orderedIndices.Length; i++) {
+                    var index = orderedIndices[i];
+                    var node = Node.children[index];
+                    yield return ExecuteNodeRoutine(node, SetState);
+
+                    // 成功していた場合、完了とする
+                    if (State == State.Success) {
+                        yield break;
+                    }
+                }
+
+                // 誰も実行できなかった場合、失敗とする
+                SetState(State.Failure);
+            }
+        }
+
+        /// <summary>
+        /// ロジックの生成
+        /// </summary>
+        public override ILogic CreateLogic(IBehaviourTreeRunner runner) {
+            return new Logic(runner, this);
+        }
+    }
+}
---
+++ Assets/GameAiBehaviour/Runtime/Nodes/RandomSequencerNode.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace GameAiBehaviour {
+    /// <summary>
+    /// ランダムシーケンスノード
+    /// </summary>
+    public sealed class RandomSequencerNode : CompositeNode {
+        private class Logic : Logic<RandomSequencerNode> {
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            public Logic(IBehaviourTreeRunner runner, RandomSequencerNode node) : base(runner, node) {
+            }
+
+            /// <summary>
+            /// 実行ルーチン
+            /// </summary>
+            protected override IEnumerator ExecuteRoutineInternal() {
+                if (Node.children.Length <= 0) {
+                    SetState(State.Failure);
+                    yield break;
+                }
+
+                // ランダムに実行
+                var orderedIndices = Enumerable.Range(0, Node.children.Length)
+                    .OrderBy(_ => Random.Range(0, 100))
+                    .ToArray();
+                
+                for (var i = 0; i < orderedIndices.Length; i++) {
+                    var index = orderedIndices[i];
+                    var node = Node.children[index];
+                    yield return ExecuteNodeRoutine(node, SetState);
+
+                    // 成功していたら待機
+                    if (State == State.Success) {
+                        // 最後だったら終わる
+                        if (i >= Node.children.Length - 1) {
+                            break;
+                        }
+
+                        yield return this;
+                    }
+                    // 失敗していた場合、そのまま失敗として終了
+                    else if (State == State.Failure) {
+                        yield break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ロジックの生成
+        /// </summary>
+        public override ILogic CreateLogic(IBehaviourTreeRunner runner) {
+            return new Logic(runner, this);
+        }
+    }
+}
